6bc25f6b94b6467feb929d46b9cb3d811dd332bc
José Luis G <josegarzaramos@gmail.com>
2021-05-01 18:11:35 -0500

    Commented weatherchart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import WeatherChart from './components/WeatherChart/WeatherChart';
+// import WeatherChart from './components/WeatherChart/WeatherChart';
 import WeatherFetch from './components/WeatherFetch/WeatherFetch';
 function App() {
   return (
